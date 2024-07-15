d2a65c1a8ac53a86e189ecdf884991c5d73bd54d
RAMAKRISHNA3488 <132547350+RAMAKRISHNA3488@users.noreply.github.com>
2024-07-15 14:45:10 +0530

    commit rk

---
+++ Prasunet_WD_05/cities.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    country:"RK",
+    name: "Kurnool",
+    lat: "15.8497",
+    lng: "78.0599",
+  },
+  {
+    country:"RK",
+    name: "Hyderabad",
+    lat: "17.3850",
+    lng: "78.4867",
+  }
+]
